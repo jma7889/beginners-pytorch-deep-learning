650d1b86347e3733dab96f1062c4701223c89f9f
Peter Gedeck <peter.gedeck@gmail.com>
2022-02-13 15:15:36 -0500

    Add docker and make it almost work with latest versions

---
+++ Makefile
@@ -0,0 +1,17 @@
+build: docker/images
+
+BOOKGROUP_IMAGE=bookgroup
+BOOKGROUP_JUPYTER=bookgroup_jupyter
+
+images:
+	docker build -t $(BOOKGROUP_IMAGE) -f docker/Dockerfile .
+	docker build -t $(BOOKGROUP_JUPYTER) -f docker/Dockerfile.jupyter .
+
+bash:
+	docker run --rm -v $(PWD):/src  -it $(BOOKGROUP_IMAGE) bash
+
+jupyter:
+	docker run --rm -v $(PWD):/src -p 8978:8978 $(BOOKGROUP_JUPYTER) jupyter notebook --allow-root --port=8978 --ip 0.0.0.0 --no-browser
+
+bash-jupyter:
+	docker exec   -it $(BOOKGROUP_JUPYTER) bash
--- chapter2/download.py
+++ chapter2/download.py
@@ -1,13 +1,13 @@
 # download.py
 
-import os
-import sys
-import urllib3
-from urllib.parse import urlparse
-import pandas as pd
 import itertools
+import os
 import shutil
+import sys
+from urllib.parse import urlparse
 
+import pandas as pd
+import urllib3
 from urllib3.util import Retry
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
@@ -15,19 +15,19 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 classes = ["cat", "fish"]
 set_types = ["train", "test", "val"]
 
+
 def download_image(url, klass, data_type):
     basename = os.path.basename(urlparse(url).path)
     filename = "{}/{}/{}".format(data_type, klass, basename)
     if not os.path.exists(filename):
-        try: 
+        try:
             http = urllib3.PoolManager(retries=Retry(connect=1, read=1, redirect=2))
-            with http.request("GET", url, preload_content=False) as resp, open(
-                filename, "wb"
-            ) as out_file:
+            with http.request("GET", url, preload_content=False) as resp, open(filename, "wb") as out_file:
                 if resp.status == 200:
                     shutil.copyfileobj(resp, out_file)
                 else:
-                    print("Error downloading {}".format(url))
+                    print(resp)
+                    print("Error writing {}".format(url))
             resp.release_conn()
         except:
             print("Error downloading {}".format(url))
@@ -49,10 +49,26 @@ if __name__ == "__main__":
 
     print("Downloading {} images".format(len(imagesDF)))
 
-    result = [
-        download_image(url, klass, data_type)
-        for url, klass, data_type in zip(
-            imagesDF["url"], imagesDF["class"], imagesDF["type"]
-        )
-    ]
+    # result = [
+    #     download_image(url, klass, data_type)
+    #     for url, klass, data_type in zip(
+    #         imagesDF["url"], imagesDF["class"], imagesDF["type"]
+    #     )
+    # ]
+    from pathlib import Path
+    missing = 0
+    zero = 0
+    available = 0
+    for url, klass, data_type in zip(imagesDF["url"], imagesDF["class"], imagesDF["type"]):
+        basename = os.path.basename(urlparse(url).path)
+        filename = Path(data_type) / klass / basename
+        if not filename.exists():
+            missing += 1
+        elif filename.stat().st_size == 0:
+            zero += 1
+            filename.unlink()
+        else:
+            available += 1
+    print(available, zero, missing)
+
     sys.exit(0)
